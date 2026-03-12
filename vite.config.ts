d24be210cdaf4693cba5aa18559599af57874c26
HuberttGit <hubert.talib@gmail.com>
2026-03-12 17:46:13 +0100

    install nitro

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,16 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import vitereact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { nitro } from 'nitro/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(), tsconfigPaths(), tanstackStart(), vitereact()],
+  plugins: [
+    tailwindcss(),
+    nitro({ preset: 'vercel' }),
+    tsconfigPaths(),
+    tanstackStart(),
+    vitereact(),
+  ],
   server: {
     host: true,
   },
