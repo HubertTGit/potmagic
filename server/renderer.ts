454dc53d9bc15d60b91a87319353757a79356ba1
HuberttGit <hubert.talib@gmail.com>
2026-03-13 05:52:21 +0100

    Remove Nitro SSR renderer and plugin, and simplify the `start` script.

--- server/renderer.ts
+++
@@ -1,5 +0,0 @@
-import { fetchViteEnv } from 'nitro/vite/runtime'
-
-export default function ssrRenderer({ req }: { req: Request }) {
-  return fetchViteEnv('ssr', req)
-}
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import vitereact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { nitro } from 'nitro/vite';
 
 export default defineConfig({
   plugins: [
