df28e4a49a34cd20fdcaebda2ea2a35854d85315
HuberttGit <hubert.talib@gmail.com>
2026-03-13 06:27:53 +0100

    feat: Implement Nitro for server-side rendering by deleting `index.html` and updating Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,23 @@
 import { defineConfig } from 'vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import vitereact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { nitro } from 'nitro/vite';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     tsconfigPaths(),
     tanstackStart(),
-    vitereact(),
+    nitro({
+      preset: 'node-server',
+    }),
   ],
   server: {
     host: true,
   },
   optimizeDeps: {
     exclude: ['fsevents', 'lightningcss'],
-  },
-  environments: {
-    client: {
-      build: {
-        outDir: '.output/public',
-      },
-    },
-    ssr: {
-      build: {
-        rollupOptions: {
-          external: ['vite'],
-        },
-      },
-    },
   },
   build: {
     rollupOptions: {
