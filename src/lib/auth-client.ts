ff006ff9f8fa68eef3de2244369f7fd5977707fb
HuberttGit <hubert.talib@gmail.com>
2026-03-13 01:47:02 +0100

    remove base url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,6 +3,6 @@ import { inferAdditionalFields } from 'better-auth/client/plugins';
 import type { auth } from '@/lib/auth';
 
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_APP_URL ?? 'http://localhost:3000',
+  //baseURL: import.meta.env.VITE_APP_URL ?? 'http://localhost:3000',
   plugins: [inferAdditionalFields<typeof auth>()],
 });
